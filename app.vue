584634792f811ed0f018b5c9db8a4e53c7a4ed90
berg <741129667@qq.com>
2021-12-29 14:36:01 +0800

    aaa

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-button type="primary" @click="handleClick">打开弹窗</el-button>
+    <eve-dialog-base title="标题" v-if="visible" @close="visible = false">
+      <div>弹框内容</div>
+    </eve-dialog-base>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.visible = true;
+    },
+  },
+};
+</script>
